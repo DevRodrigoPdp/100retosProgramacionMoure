735e46e3952f4e3ac15cef563fd8e21a510f5154
Rodrigo Puerta <rpuertadelpozo@gmail.com>
2025-03-05 18:25:12 +0100

    Update Stats

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 100retosProgramacionMoure
+
+Aqui es donde iré haciendo los retos de programacion para poder aprender bien JavaScript
---
+++ sintaxis-variables-tiposDato-holaMundo.js
@@ -0,0 +1,22 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+// Una linea
+/* 
+    Varias 
+    lineas
+*/
+
+const nombre = "Rodrigo"
+let edad = "25"
+
+string ="Rodrigo Puerta Del Pozo"
+boolean ="true"
+number = '25'
+undefined = "undefined"
+null = "null"
+BigInt = "23432424"
+Symbol = ""
+
+console.log("¡Hola , Javascript!")
+
+
